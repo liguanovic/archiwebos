93e13e089bee2e7afe9505b23d4b7bf1631447ed
liguanovic <liguanovic787878@gmail.com>
2024-05-15 18:18:32 +0400

    add function for filters and add function to sort by filters

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -3,6 +3,7 @@
 // ***** CONSTANTS ***** //
 
 const galleryContainer = document.querySelector(".gallery");
+const categoriesContainer = document.querySelector(".filters");
 
 // ***** VARIABLES ***** //
 
@@ -26,7 +27,8 @@ function addWorksToHTML(works) {
     const imgElement = document.createElement("img");
     const figcaptionElement = document.createElement("figcaption");
 
-    figureElement.id = work.id;
+    figureElement.id = work.categoryId;
+    figureElement.classList.add("works");
     imgElement.src = work.imageUrl;
     imgElement.alt = work.title;
     figcaptionElement.innerText = work.title;
@@ -38,9 +40,56 @@ function addWorksToHTML(works) {
   };
 }
 
-async function main(){
+async function fetchFilters() {
+  const url = "http://localhost:5678/api/categories";
+
+  const response = await fetch(url);
+  let categories = await response.json();
+  console.log("Categories :", categories);
+  return categories;
+}
+
+function addFilters(categories) {
+  for (const category of categories) {
+    const li = document.createElement("li");
+    const filter = document.createElement("button");
+
+    filter.id = category.id;
+    filter.innerText = category.name;
+    filter.classList.add("filters");
+
+    li.appendChild(filter);
+    categoriesContainer.appendChild(li);
+  };
+
+  filterWorks();
+}
+
+function filterWorks() {
+  categoriesContainer.addEventListener("click", (event) => {
+    const clickedCategoryId = event.target.id;
+
+    if (!clickedCategoryId) return;
+
+    const works = document.querySelectorAll(".works");
+    
+    works.forEach(work => {
+      if (clickedCategoryId === "all" ? true : work.id === clickedCategoryId) {
+        work.style.display = "block";
+      } else {
+        work.style.display = "none";
+      }
+    });
+  });
+}
+
+
+async function main() {
   let works = await fetchWorks();
   addWorksToHTML(works);
+
+  let categories = await fetchFilters();
+  addFilters(categories);
 }
 
 main();
